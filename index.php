34c61da586290ecb8a56c3f1b95bd7799716ef99
Roni Laukkarinen <roni@dude.fi>
2021-05-16 22:41:36 +0300

    Backup

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// Irssi log parser
+function strposa( $haystack, $needles = array(), $offset = 0 ) {
+  $chr = array();
+  foreach( $needles as $needle ) {
+    $res = strpos($haystack, $needle, $offset);
+    if ($res !== false) $chr[$needle] = $res;
+  }
+
+  if ( empty( $chr ) ) return false;
+  return min( $chr );
+}
+
+$logfile = fopen("all.log", "r");
+
+echo "categories:
+- AI
+- artificial intelligence
+conversations:
+";
+
+if ( $logfile ) {
+  $i = 0;
+  while ( ( $line = fgets( $logfile ) ) !== false) {
+
+    // Remove lines that contain actions
+    $ignored = array( 'opened', '-!-', 'Users', '[@', '!säännöt', ' * ' );
+    $regex_find = array( '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/i', '/\<(.*?)\>/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+:( )/i', '/!+[äöåÄÖÅa-zA-ZäöåÄÖÅ]/i' );
+    $regex_replace = array( '', '', '', '' );
+    
+    if ( ! strposa( $line, $ignored, 1 ) ) {
+
+      $processed_output = preg_replace( $regex_find, $regex_replace, $line );
+      $caps = ucfirst( substr( $processed_output, 2) );
+
+      if ($i % 2 == 0 ) {
+        echo '  - ' . $caps;
+      } else {
+        echo '- - ' . $caps;
+      }
+
+    }
+
+    $i++;
+  }
+
+fclose( $logfile );
+}
