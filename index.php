5fda3d2b3f874e8a8053f991138ecd834a81b926
Roni Laukkarinen <roni@dude.fi>
2021-05-17 20:15:40 +0300

    Clean ups

--- index.php
+++ index.php
@@ -21,15 +21,26 @@ if ( $logfile ) {
 
     // Remove lines that contain actions
     $ignored = array( 'opened', '-!-', 'Users', '[@', '!säännöt', ' * ', 'Topic', 'topic', '@', 'http', '’', 'changed', 'Changed', '---', ',,', '__', '>>', '[', ']', 'Jape`:', '`' );
-    $regex_find = array( '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/i', '/\<(.*?)\>/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+:( )/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+,( )/i', '/!+[äöåÄÖÅa-zA-ZäöåÄÖÅ]/i', '/"/', '/Ã¶/', '/Ã€/' );
-    $regex_replace = array( '', '', '', '', '', '\"', 'ö', 'ä' );
+    $regex_find = array( '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/i', '/\<(.*?)\>/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+:( )/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+,( )/i', '/!+[äöåÄÖÅa-zA-ZäöåÄÖÅ]/i', '/"/', '/Ã¶/', '/Ã€/', '/Ã/', '/Â/', '/\:\\\\/' );
+    $regex_replace = array( '', '', '', '', '', '\"', 'ö', 'ä', 'ö ', '', ":/" );
     
     if ( ! strposa( $line, $ignored, 1 ) ) {
 
       $processed_output = preg_replace( $regex_find, $regex_replace, $line );
       $caps = ucfirst( substr( $processed_output, 2) );
 
-        echo '"' . preg_replace( "/\r|\n/", "", $caps ) . '",';
+        // Remove line breaks
+        $remove_line_breaks = preg_replace( "/\r|\n/", "", $caps );
+
+        // Remove diamond question marks
+        $remove_diamonds = preg_replace( "/\x{FFFD}/u", "", $remove_line_breaks );
+
+        // Output with double quotes
+        $output = '"' . $remove_diamonds . '",';
+
+        // Remove mapping values from from output
+        echo preg_replace( '/\"\./', '"', $output );
+
         echo "\r\n";
 
     }
