466506c5e2d0156f7b814c3bf48647ae5a3b8692
Roni Laukkarinen <roni@dude.fi>
2021-05-17 19:12:26 +0300

    Remove line breaks

--- index.php
+++ index.php
@@ -10,33 +10,8 @@ function strposa( $haystack, $needles = array(), $offset = 0 ) {
   if ( empty( $chr ) ) return false;
   return min( $chr );
 }
-
 $logfile = fopen("all.log", "r");
-
 echo 'conversation = [';
-
-if ( $logfile ) {
-  $i = 0;
-  while ( ( $line = fgets( $logfile ) ) !== false) {
-
-    // Remove lines that contain actions
-    $ignored = array( 'opened', '-!-', 'Users', '[@', '!säännöt', ' * ', 'Topic', 'topic', '@', 'http', '’', 'changed', 'Changed', '---', ',,', '__', '>>', '[', ']', 'Jape`:', '`' );
-    $regex_find = array( '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/i', '/\<(.*?)\>/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+:( )/i', '/!+[äöåÄÖÅa-zA-ZäöåÄÖÅ]/i', '/"/' );
-    $regex_replace = array( '', '', '', '', '\"' );
-    
-    if ( ! strposa( $line, $ignored, 1 ) ) {
-
-      $processed_output = preg_replace( $regex_find, $regex_replace, $line );
-      $caps = ucfirst( substr( $processed_output, 2) );
-
-        echo '"' . $caps . '",';
-
-    }
-
-    $i++;
-  }
-
-fclose( $logfile );
-}
-
+if ( $logfile ) { $i = 0; while ( ( $line = fgets( $logfile ) ) !== false) { $ignored = array( 'opened', '-!-', 'Users', '[@', '!säännöt', ' * ', 'Topic', 'topic', '@', 'http', '’', 'changed', 'Changed', '---', ',,', '__', '>>', '[', ']', 'Jape`:', '`' );
+    $regex_find = array( '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/i', '/\<(.*?)\>/i', '/[a-zA-ZäöåÄÖÅ0-9_-]+:( )/i', '/!+[äöåÄÖÅa-zA-ZäöåÄÖÅ]/i', '/"/' ); $regex_replace = array( '', '', '', '', '\"' ); if ( ! strposa( $line, $ignored, 1 ) ) { $processed_output = preg_replace( $regex_find, $regex_replace, $line ); $caps = ucfirst( substr( $processed_output, 2) ); echo '"' . $caps . '",'; } $i++; } fclose( $logfile ); }
 echo ']';
